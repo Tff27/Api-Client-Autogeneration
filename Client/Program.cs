35569e1553d4c550f293759b18a192a2b1b97789
Tiago Fonseca <tiagoffonseca@outlook.pt>
2021-12-07 01:06:24 +0000

    .net 6 program structure

--- Client/Program.cs
+++ Client/Program.cs
@@ -1,32 +1,24 @@
-﻿namespace Client
+﻿using SDK;
+using System;
+using System.Net.Http;
+
+Console.WriteLine("Welcome to Nswag Generated Client");
+Console.WriteLine("Press any key to fetch data");
+Console.ReadLine();
+
+// Created object of class ValuesClient  
+
+var httpClient = new HttpClient();
+var testClient = new WeatherServiceClientSDK("http://localhost:49183", httpClient);
+
+// Call GetAsync Values API  
+var getresult = await testClient.WeatherForecastAsync();
+
+// Call GetAllAsync Values API  
+
+foreach (var item in getresult)
 {
-    using SDK;
-    using System;
-    using System.Net.Http;
-
-    class Program
-    {
-        static async System.Threading.Tasks.Task Main(string[] args)
-        {
-            Console.WriteLine("Welcome to Nswag Generated Client");
-            Console.ReadLine();
-
-            // Created object of class ValuesClient  
-
-            var httpClient = new HttpClient();
-            var testClient = new WeatherServiceClientSDK("http://localhost:49183", httpClient);
-
-            // Call GetAsync Values API  
-            var getresult = await testClient.WeatherForecastAsync();
-
-            // Call GetAllAsync Values API  
-
-            foreach (var item in getresult)
-            {
-                Console.WriteLine($"Weather {item.Summary} is {item.TemperatureC}º/{item.TemperatureF}F on {item.Date}");
-            }
-
-            Console.ReadLine();
-        }
-    }
+    Console.WriteLine($"Weather {item.Summary} is {item.TemperatureC}º/{item.TemperatureF}F on {item.Date}");
 }
+
+Console.ReadLine();
